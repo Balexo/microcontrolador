0c3fb4191e56701c4465262713c72c48ec91ffb5
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-11-27 18:00:18 +0100

    Añadida la funcion Print

--- millis.ino
+++ millis.ino
@@ -6,6 +6,14 @@ int dosSegundos= 2000;
 int previousMillisCtr1 =0;
 int previousMillisCtr2 =0;
 
+ void Print( int ctr1, int ctr2){
+    Serial.print("C1 : ");
+    ctr1++;
+    Serial.print(ctr1);
+    Serial.print(" C2 : ");
+    Serial.println(ctr2);
+  }
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -16,21 +24,13 @@ void loop() {
  
   if(currentMillis - previousMillisCtr1 >= decimoSegundo){
     previousMillisCtr1 = currentMillis;
-    Serial.print("C1 : ");
-    ctr1++;
-    Serial.print(ctr1);
-    Serial.print(" C2 : ");
-    Serial.println(ctr2);
+    Print(ctr1, ctr2);
   }
     
   if(currentMillis - previousMillisCtr2 >= dosSegundos){
     previousMillisCtr2 = currentMillis;
   ctr2++;
   ctr1 -= ctr2;
-  Serial.println("2 segundos");
-  Serial.print("C1 : ");
-  Serial.print(ctr1);
-  Serial.print(" C2 : ");
-  Serial.println(ctr2);
+  Print(ctr1, ctr2);
   }
 }
