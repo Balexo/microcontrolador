290342c8de46d0c35a8a60f8f950007cad44d2f5
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-11-28 14:27:28 +0100

    dealy2 and millis added

--- delay2_.ino
+++ delay2_.ino
@@ -8,21 +8,22 @@ void setup() {
 
 void loop() {
  
-  for(int i=0 ; i<10; i++){
-    Serial.print("C1 : ");
-    ctr1++;
-    Serial.print(ctr1);
-    Serial.print(" C2 : ");
-    Serial.println(ctr2);
-    delay(100);
-  }
-
-  ctr2++;
-  ctr1 -= ctr2;
-  Serial.println("2 segundos");
+  ctr1++;
   Serial.print("C1 : ");
   Serial.print(ctr1);
   Serial.print(" C2 : ");
   Serial.println(ctr2);
-  delay(2000);
+
+  delay(100);
+
+ if(ctr1%20==0){
+    ctr2++;
+    ctr1 -= ctr2;
+
+    Serial.println("2 segundos");
+    Serial.print("C1 : ");
+    Serial.print(ctr1);
+    Serial.print(" C2 : ");
+    Serial.println(ctr2);
+  }
 }
--- millis.ino
+++ millis.ino
@@ -1,17 +1,10 @@
 int ctr1 =0; 
-int ctr2 =0;
-int initialMillis =0;
-int decimoSegundo = 100;
-int dosSegundos= 2000;
-int previousMillisCtr1 =0;
-int previousMillisCtr2 =0;
+int unSegundo= 1000;
+unsigned long previousMillisCtr1 =0;
 
- void Print( int ctr1, int ctr2){
-    Serial.print("C1 : ");
-    ctr1++;
-    Serial.print(ctr1);
-    Serial.print(" C2 : ");
-    Serial.println(ctr2);
+
+ void Print( int ctr1){
+    Serial.println(" C1 : " + String(ctr1));
   }
 
 void setup() {
@@ -20,17 +13,12 @@ void setup() {
 }
 
 void loop() {
-  int currentMillis = millis();
+  unsigned long currentMillis = millis();
  
-  if(currentMillis - previousMillisCtr1 >= decimoSegundo){
+  if(currentMillis - previousMillisCtr1 >= unSegundo){
     previousMillisCtr1 = currentMillis;
-    Print(ctr1, ctr2);
+    Print(ctr1);
+    ctr1++;
   }
     
-  if(currentMillis - previousMillisCtr2 >= dosSegundos){
-    previousMillisCtr2 = currentMillis;
-  ctr2++;
-  ctr1 -= ctr2;
-  Print(ctr1, ctr2);
-  }
 }
